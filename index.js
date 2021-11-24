b5be6e4bc8d92b907eb2c0b9424d45e9521b59c5
Danusa Jesus <85651340+DanSiljer@users.noreply.github.com>
2021-11-23 22:26:36 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+
+}
+
 const express = require('express');
 const cors  = require('cors');
 const TodolistRouter = require('./routes/todolist.routes');
@@ -10,9 +15,15 @@ app.use(cors());
 
 app.use('/todolist', TodolistRouter);
 
-Conn();
+// buscando os dados da variavel de ambiente
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+Conn(db_url, db_user, db_pass, db_data);
 
+// inicializar o servidor http em alguma porta para podermos acessar ele.
 const port = 3001;
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`O servidor esta rodando na porta ${port}`);
 })
